8bdc6c09b7f20556f5f24a31319d16979f63748a
amankhan81 <123284152+amankhan81@users.noreply.github.com>
2025-10-13 09:38:45 +0300

    1

---
+++ story_content/user.js
@@ -0,0 +1,79 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5nLbjwGJihh":
+        Script1();
+        break;
+      case "6GvvlwbtYmD":
+        Script2();
+        break;
+  }
+}
+
+function Script1()
+{
+  var elem = document.documentElement;
+
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) { /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE11 */
+    document.msExitFullscreen();
+  }
+}
+
+
+
+if(document.fullscreenElement == null){
+openFullscreen();
+}else{
+closeFullscreen();
+};
+}
+
+function Script2()
+{
+  var elem = document.documentElement;
+
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) { /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE11 */
+    document.msExitFullscreen();
+  }
+}
+
+
+
+if(document.fullscreenElement == null){
+openFullscreen();
+}else{
+closeFullscreen();
+};
+}
+
