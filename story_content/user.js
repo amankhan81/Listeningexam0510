3ef7997d210a1d43827eeea62e692b7a7b556ef3
amankhan81 <123284152+amankhan81@users.noreply.github.com>
2025-10-14 16:06:52 +0300

    6

--- story_content/user.js
+++ story_content/user.js
@@ -2,14 +2,17 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "69ByFdS4lIp":
+      case "6ftLcARFlg1":
         Script1();
         break;
-      case "6RW9VA7Da7u":
+      case "6K4hHiREUXl":
         Script2();
         break;
-      case "5q7LRMmKseX":
+      case "6HRQWOS0cmM":
         Script3();
+        break;
+      case "6hxT5iiTMGB":
+        Script4();
         break;
   }
 }
@@ -81,6 +84,39 @@ closeFullscreen();
 }
 
 function Script3()
+{
+  var elem = document.documentElement;
+
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) { /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE11 */
+    document.msExitFullscreen();
+  }
+}
+
+
+
+if(document.fullscreenElement == null){
+openFullscreen();
+}else{
+closeFullscreen();
+};
+}
+
+function Script4()
 {
   var elem = document.documentElement;
 
