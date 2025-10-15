ee816a88c5c76ceb7c1746c9683b269e1b711b36
amankhan81 <123284152+amankhan81@users.noreply.github.com>
2025-10-15 12:07:35 +0300

    2

--- story_content/user.js
+++ story_content/user.js
@@ -2,20 +2,26 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6d0aWGAXmfZ":
+      case "6UzXcg3EY7f":
         Script1();
         break;
-      case "6gQEJHNCHl6":
+      case "6Etpub8btfo":
         Script2();
         break;
-      case "5lIyCZLuQXf":
+      case "6Z4gshNu1wc":
         Script3();
         break;
-      case "5egsxxPlIgL":
+      case "6TY96VWUDOr":
         Script4();
         break;
-      case "5bxI1iWimR6":
+      case "5umDFYmuOQR":
         Script5();
+        break;
+      case "6o02TsNJ5FI":
+        Script6();
+        break;
+      case "5imSKXXo9LT":
+        Script7();
         break;
   }
 }
@@ -153,6 +159,72 @@ closeFullscreen();
 }
 
 function Script5()
+{
+  var elem = document.documentElement;
+
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) { /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE11 */
+    document.msExitFullscreen();
+  }
+}
+
+
+
+if(document.fullscreenElement == null){
+openFullscreen();
+}else{
+closeFullscreen();
+};
+}
+
+function Script6()
+{
+  var elem = document.documentElement;
+
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) { /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE11 */
+    document.msExitFullscreen();
+  }
+}
+
+
+
+if(document.fullscreenElement == null){
+openFullscreen();
+}else{
+closeFullscreen();
+};
+}
+
+function Script7()
 {
   var elem = document.documentElement;
 
