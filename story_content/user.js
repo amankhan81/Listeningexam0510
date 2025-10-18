a5a98623abe87a2c9c792d7e4da041e44372f324
amankhan81 <123284152+amankhan81@users.noreply.github.com>
2025-10-18 20:47:05 +0300

    2

--- story_content/user.js
+++ story_content/user.js
@@ -2,29 +2,32 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6WiXHYYCsNk":
+      case "6Er3W8pyWzF":
         Script1();
         break;
-      case "6RgSmjxnvUo":
+      case "6U66gMrlKRg":
         Script2();
         break;
-      case "6kxagoQTLNZ":
+      case "5aRP4EQMIwP":
         Script3();
         break;
-      case "6Aw1ljv4oXP":
+      case "6NBVea7KUJL":
         Script4();
         break;
-      case "5krXTSdGUDm":
+      case "6HDF4Ue0Haa":
         Script5();
         break;
-      case "5eh3GiOgKwR":
+      case "5gzhFpDXlYN":
         Script6();
         break;
-      case "6ZMLoYLpbCV":
+      case "5giIR08m4Ja":
         Script7();
         break;
-      case "5e1loktd4Kg":
+      case "6PSJQPZLRdZ":
         Script8();
+        break;
+      case "6jFxtBJ0wUO":
+        Script9();
         break;
   }
 }
@@ -261,6 +264,39 @@ closeFullscreen();
 }
 
 function Script8()
+{
+  var elem = document.documentElement;
+
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) { /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE11 */
+    document.msExitFullscreen();
+  }
+}
+
+
+
+if(document.fullscreenElement == null){
+openFullscreen();
+}else{
+closeFullscreen();
+};
+}
+
+function Script9()
 {
   var elem = document.documentElement;
 
