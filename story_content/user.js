fc44abe8e404873666ae70ecc35f0bee487ce395
amankhan81 <123284152+amankhan81@users.noreply.github.com>
2025-10-18 20:57:00 +0300

    exam25

--- story_content/user.js
+++ story_content/user.js
@@ -2,31 +2,31 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6Er3W8pyWzF":
+      case "64KzHr3Pilb":
         Script1();
         break;
-      case "6U66gMrlKRg":
+      case "63eY2f30xHP":
         Script2();
         break;
-      case "5aRP4EQMIwP":
+      case "6O4DbJFmvg0":
         Script3();
         break;
-      case "6NBVea7KUJL":
+      case "5jFvDytY8vE":
         Script4();
         break;
-      case "6HDF4Ue0Haa":
+      case "5faa0O2cXB9":
         Script5();
         break;
-      case "5gzhFpDXlYN":
+      case "6k4lQIhKSwA":
         Script6();
         break;
-      case "5giIR08m4Ja":
+      case "60GtGETPvJS":
         Script7();
         break;
-      case "6PSJQPZLRdZ":
+      case "5mFy6JYqqL6":
         Script8();
         break;
-      case "6jFxtBJ0wUO":
+      case "63C9tDW4t0y":
         Script9();
         break;
   }
