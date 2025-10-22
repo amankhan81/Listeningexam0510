c1d0ae631f48887f6c360e2d8ac459b36e1b4627
amankhan81 <123284152+amankhan81@users.noreply.github.com>
2025-10-22 11:36:49 +0300

    2

--- story_content/user.js
+++ story_content/user.js
@@ -2,35 +2,38 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "5oYTarnlKNH":
+      case "5exIFlj4IkH":
         Script1();
         break;
-      case "6ezkosFSOnT":
+      case "5an3Ql0uMa5":
         Script2();
         break;
-      case "5xqQawenwKo":
+      case "6qRVdS7BHMe":
         Script3();
         break;
-      case "6f19haT0dxm":
+      case "6bfectAWreC":
         Script4();
         break;
-      case "5fb0RIoVuck":
+      case "6A8hCWzJtmN":
         Script5();
         break;
-      case "6GLEcgSxu4b":
+      case "6HJfDb5Ma8S":
         Script6();
         break;
-      case "5lwWwgdahUh":
+      case "5XXTScIvydh":
         Script7();
         break;
-      case "6eCxEmmyXjn":
+      case "6D87BTUtjP0":
         Script8();
         break;
-      case "5xvNtC4QsNh":
+      case "5obaywrfoBf":
         Script9();
         break;
-      case "6RGSLVj2xB3":
+      case "5dOBZIF2K2F":
         Script10();
+        break;
+      case "6RzPgW7Gpl7":
+        Script11();
         break;
   }
 }
@@ -333,6 +336,39 @@ closeFullscreen();
 }
 
 function Script10()
+{
+  var elem = document.documentElement;
+
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) { /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE11 */
+    document.msExitFullscreen();
+  }
+}
+
+
+
+if(document.fullscreenElement == null){
+openFullscreen();
+}else{
+closeFullscreen();
+};
+}
+
+function Script11()
 {
   var elem = document.documentElement;
 
